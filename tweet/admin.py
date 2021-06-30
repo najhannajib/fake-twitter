c835d1ed790cd2fbf822d93e8ef023ec5a7896c2
PNSB <pnsb@PNSBs-MacBook-Air.local>
2021-06-30 12:59:58 +0800

    first commit

---
+++ tweet/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from tweet.models import Post, Comment, Preference
+
+# Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Preference)
